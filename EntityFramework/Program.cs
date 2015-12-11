65ba02b4c6a448a6cc16d37f88168c4ac030ff22
lester88a <lester88a@gmail.com>
2015-12-11 14:40:23 -0500

    entity framework

---
+++ EntityFramework/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EntityFramework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+             * Entity Framework
+             *  object related mapper
+             *  eliminates the need for data-access code in some situation
+             * 
+             * differen sernarios
+             *  1.designer - centic
+             *  2.code centic
+             */
+        }
+    }
+}
